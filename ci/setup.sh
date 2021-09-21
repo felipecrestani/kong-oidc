288c2b288d0c45d999ea94be4da8cdcd4ca2eb90
Felipe Cesar Crestani <felipe.crestani@contaazul.com>
2021-09-20 22:01:50 -0300

    Add zlib to compress session size

--- ci/setup.sh
+++ ci/setup.sh
@@ -14,4 +14,5 @@ luarocks install lua-resty-openidc ${LUA_RESTY_OPENIDC_VERSION}
 luarocks install lua-cjson
 luarocks install luaunit
 luarocks install luacov
-
+luarocks install lua-ffi-zlib
+luarocks install penlight
